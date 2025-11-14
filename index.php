34eade45af546b7800ce866015eadbf89c3130f3
Gustavo-Labgo <gustavoh2025@outlook.fr>
2025-11-14 17:46:47 -0500

    Ajout des fichierss

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+// Afficher l’image depuis le dossier du thème
+echo '<img src="' . get_template_directory_uri() . '/inicio.png" alt="home" style="width:500px; display:block; margin:0 auto;">';
